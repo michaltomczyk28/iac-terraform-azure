2a820b933562cf8e176e264487c26f6f5bddb869
Michal Tomczyk <michaltomczyk28@gmail.com>
2023-05-21 22:45:48 +0200

    feat: add terraform configuration for azure functions app service plan

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -10,3 +10,14 @@ resource "azurerm_storage_account" "tf_fun_sa" {
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_app_service_plan" "tf_fun_asp" {
+  name                = "asp-tffunctions"
+  location            = azurerm_resource_group.tf_fun_rg.location
+  resource_group_name = azurerm_resource_group.tf_fun_rg.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
